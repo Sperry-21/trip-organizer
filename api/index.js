8805fc35dc4f8c36eb0a91d62d60930b2792f5c1
Sam Perry <sperry023@gmail.com>
2026-07-23 12:38:32 -0400

    Restore Express server with all API endpoints

--- api/index.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'public', 'index.html');
-  const html = fs.readFileSync(filePath, 'utf-8');
-  res.setHeader('Content-Type', 'text/html');
-  res.send(html);
-}
